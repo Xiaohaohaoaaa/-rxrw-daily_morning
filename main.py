b24bd869a0987bbb2463c6e7421317366a201f86
Xiaohaohaoaaa <113002359+Xiaohaohaoaaa@users.noreply.github.com>
2023-03-31 13:23:28 +0800

    Update main.py
    修改查询天气接口

--- main.py
+++ main.py
@@ -19,10 +19,9 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  url = "https://v0.yiketianqi.com/api?unescape=1&version=v61&appid=43656176&appsecret=I42og6Lm&ext=&cityid=&city=" + city
   res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  return res['wea'], math.floor(res['tem'])
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
